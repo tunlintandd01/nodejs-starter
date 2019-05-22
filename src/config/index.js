6751518aa0e4215ea8a0a30576bc1de4d0d13690
谭暾林pascal <50311177+tunlintandd01@users.noreply.github.com>
2019-05-22 17:44:41 +0800

    perf: update config

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,22 @@
 'use strict'
 
-const _ = require('lodash')
-// default is "development" environment
-const envConfig = require(`./config.${process.env.NODE_ENV || 'development'}`)
-const commonConfig = require('./config.common')
-module.exports = _.merge(commonConfig, envConfig)
+const convict = require('convict')
+const path = require('path')
+const yaml = require('js-yaml')
+
+convict.addParser({
+  extension: 'yml',
+  parse: yaml.safeLoad,
+})
+
+const config = convict(path.join(__dirname, 'configSchema.yml'))
+const env = config.get('env')
+
+config.loadFile([
+  path.join(__dirname, `${env}.yml`),
+  path.join(__dirname, `credentials.${env}.yml`),
+])
+
+config.validate({ allowed: 'warn' })
+
+module.exports = config
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const { createApiServer } = require('./app')
 const config = require('./config')
+const logger = require('./utils/logger')
 
 async function main() {
   const app = await createApiServer()
-  app.listen(config.port)
+  const port = config.get('port')
+  app.listen(port)
+  logger.debug(`server listening to ${port}...`)
 }
 
 main()
